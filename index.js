51b012eca9cea549f4fe8b6728a81903dee6e161
Amir <amiraglodiya01@gmail.com>
2025-08-22 13:29:35 +0530

    Updated all

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const path = require("path");
 
 dotenv.config();
 const app = express();
@@ -14,13 +13,6 @@ app.use(express.json());
 // Routes
 app.use("/api/form/contact", require("./routes/contact"));
 
-// Serve React build in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
 
 // Root route (important ✅)
 app.get("/", (req, res) => {
